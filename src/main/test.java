76798a1d75ce9c8d1476400a60e5a8b885c510e8
songyanji <songyanji123@163.com>
2019-11-10 22:55:28 +0800

    2.0

--- src/main/test.java
+++ src/main/test.java
@@ -3,6 +3,8 @@ package main;
 public class test {
     public static void main(String args){
         System.out.print("syjnb");
+        System.out.print("syjnb2");
+        System.out.print("syjnb3");
         int a = 1;
     }
 }
